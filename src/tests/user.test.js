0cdd75d2f116ec4499a3d10ce259b9dc0130f341
RobinOShiaa <robin.oshea23@mail.dcu.ie>
2021-08-30 15:44:39 +0100

    test change

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -1,5 +1,22 @@
 const request = require('supertest')
 const app = require('../app')
+const User = require('../models/user')
+
+
+const userOne = {
+  name : 'Test Robin',
+  email : 'Test@gmail.com',
+  password : '123456789'
+}
+
+beforeEach(async () => {
+  await User.deleteMany()
+  await new User(userOne).save()
+})
+
+// afterEach(() => {
+//   console.log('afterEach')
+// })
 
 test('Create new user' , async () => {
   await request(app).post('/users').send({
@@ -8,4 +25,20 @@ test('Create new user' , async () => {
     password : '123456789'
   }).expect(201)
 
+})
+
+
+test('Login', async () => {
+  await request(app).post('/users/login').send({
+    email : userOne.email,
+    password : userOne.password
+  }).expect(200)
+})
+
+
+test('Login failure', async () => {
+  await request(app).post('/users/login').send({
+    email : userOne.email,
+    password : '2527234637'
+  }).expect(400)
 })
